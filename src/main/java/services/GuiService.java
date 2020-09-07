f8f9caf4e5adb6cd342cee2c4e3e0e35a35ec841
hooman734 <hesamyan@outlook.com>
2020-09-07 22:56:07 +0400

    make gui working

--- src/main/java/services/GuiService.java
+++ src/main/java/services/GuiService.java
@@ -3,6 +3,7 @@ package services;
 import javazoom.jl.decoder.JavaLayerException;
 
 import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
 
 public class GuiService {
@@ -13,9 +14,27 @@ public class GuiService {
     }
 
     public void init() {
-        JFrame frame = new JFrame();
+        // creating the frame
+        JFrame frame = new JFrame("Stream Subscription");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(500, 200);
+
+        // creating the MenuBar and adding components
+        JMenuBar mb = new JMenuBar();
+        JMenu m1 = new JMenu("FILE");
+        JMenu m2 = new JMenu("Help");
+        mb.add(m1);
+        mb.add(m2);
+        JMenuItem m11 = new JMenuItem("Open");
+        JMenuItem m22 = new JMenuItem("Save as");
+        m1.add(m11);
+        m1.add(m22);
+
+        // creating the panel
         JPanel panel = new JPanel();
+        JLabel label = new JLabel("Some Text");
         JButton skipButton = new JButton("Skip");
+
 
         skipButton.addActionListener(actionEvent -> {
             try {
@@ -24,14 +43,17 @@ public class GuiService {
                 e.printStackTrace();
             }
         });
+
+        panel.add(label);
         panel.add(skipButton);
 
-        GroupLayout layout = new GroupLayout(panel);
-        panel.setLayout(layout);
+//        GroupLayout layout = new GroupLayout(panel);  // problematic!
+//        panel.setLayout(layout);
 
-        frame.getContentPane().add(panel);
-        frame.setSize(500, 200);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.getContentPane().add(BorderLayout.NORTH, mb);
+        frame.getContentPane().add(BorderLayout.CENTER, panel);
+
+
         frame.setVisible(true);
     }
 }
