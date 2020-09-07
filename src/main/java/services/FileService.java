c7f1efe6a7299f6ccffb69185210ea430c3362f1
Amir <hesamian@uwm.edu>
2020-09-07 02:27:06 -0500

    more progress

--- src/main/java/services/FileService.java
+++ src/main/java/services/FileService.java
@@ -5,7 +5,7 @@ import java.util.Base64;
 
 public class FileService {
     public byte[] base64ToStream(String base64) throws UnsupportedEncodingException {
-        byte[] decodedString = Base64.getDecoder().decode(base64.getBytes("UTF-16"));
+        byte[] decodedString = Base64.getDecoder().decode(base64.getBytes("UTF-8"));
 
         return decodedString;
     }
